314fa74501e3b9c1f45e5cc2284ac217bd304c4f
362Sadie-F <princessmae.fleming@gmail.com>
2026-02-11 12:43:05 -0500

    part 3 done

--- Topic3VariablesAssignment/Program.cs
+++ Topic3VariablesAssignment/Program.cs
@@ -11,6 +11,7 @@
             int graduationYear = 2027;
             int r = 3;
             double area = (Math.PI * r * r);
+            double rounded = Math.Round(area, 2);
             Console.WriteLine("Class is in room " + roomNumber + "!");
             Console.WriteLine("My favorite class is " + courseName + ".");
             Console.WriteLine("The price of my favorite book is " + price + ".");
@@ -18,7 +19,7 @@
             Console.WriteLine("My name is " + fullName + " and I will garduate in " + graduationYear + "!");
             Console.WriteLine("    ");
             Console.WriteLine(Math.PI * r * r); // Pi * R^2
-            Console.WriteLine("The area is " + area);
+            Console.WriteLine("The area is " + rounded);
         }
     }
 }
