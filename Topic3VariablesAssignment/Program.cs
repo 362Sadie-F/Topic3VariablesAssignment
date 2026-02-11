ae2ddff03fa3530ef0f7674d02a27c10d15b855a
362Sadie-F <princessmae.fleming@gmail.com>
2026-02-11 12:12:18 -0500

    Part 1

--- Topic3VariablesAssignment/Program.cs
+++ Topic3VariablesAssignment/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int roomNumber = 37;
+            string courseName = "Visual Arts";
+            double price = 24.95;
+            Console.WriteLine("Class is in room " + roomNumber + "!");
+            Console.WriteLine("My favorite class is " + courseName + ".");
+            Console.WriteLine("The price of my favorite book is " + price + ".");
         }
     }
 }
