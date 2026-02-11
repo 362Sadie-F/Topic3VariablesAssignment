0fb1cfe49b7f7d9a02da0d62d090c4b6dde0becc
362Sadie-F <princessmae.fleming@gmail.com>
2026-02-11 12:15:58 -0500

    part 2

--- Topic3VariablesAssignment/Program.cs
+++ Topic3VariablesAssignment/Program.cs
@@ -7,9 +7,14 @@
             int roomNumber = 37;
             string courseName = "Visual Arts";
             double price = 24.95;
+            string fullName = "Mercedes Fleming";
+            int graduationYear = 2027;
             Console.WriteLine("Class is in room " + roomNumber + "!");
             Console.WriteLine("My favorite class is " + courseName + ".");
             Console.WriteLine("The price of my favorite book is " + price + ".");
+            Console.WriteLine("    ");
+            Console.WriteLine("My name is " + fullName + " and I will garduate in " + graduationYear + "!");
+            Console.WriteLine();
         }
     }
 }
