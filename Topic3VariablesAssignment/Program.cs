c1e043be1d464500f046c6d8f04128b63364542d
362Sadie-F <princessmae.fleming@gmail.com>
2026-02-11 12:50:16 -0500

    part 4 start

--- Topic3VariablesAssignment/Program.cs
+++ Topic3VariablesAssignment/Program.cs
@@ -6,20 +6,23 @@
         {
             int roomNumber = 37;
             string courseName = "Visual Arts";
-            double price = 24.95;
+            double price = 24.95 * 1.13;
+            double priceRounded = Math.Round(price, 2);
             string fullName = "Mercedes Fleming";
             int graduationYear = 2027;
             int r = 3;
-            double area = (Math.PI * r * r);
+            double area = (Math.PI * (r * r));
             double rounded = Math.Round(area, 2);
             Console.WriteLine("Class is in room " + roomNumber + "!");
             Console.WriteLine("My favorite class is " + courseName + ".");
-            Console.WriteLine("The price of my favorite book is " + price + ".");
+            Console.WriteLine("The price of my favorite book is " + "$" + priceRounded + ".");
             Console.WriteLine("    ");
-            Console.WriteLine("My name is " + fullName + " and I will garduate in " + graduationYear + "!");
+            Console.WriteLine("My name is " + fullName + " and I will graduate in " + graduationYear + "!");
             Console.WriteLine("    ");
-            Console.WriteLine(Math.PI * r * r); // Pi * R^2
-            Console.WriteLine("The area is " + rounded);
+            Console.WriteLine(Math.PI * (r * r)); // Pi * R^2
+            Console.WriteLine("The area is " + rounded + " cm^2");
+            Console.WriteLine("    ");
+            Console.WriteLine();
         }
     }
 }
