5ca3354ac272056622aa7ed46e5e342b7c1e1ff4
362Sadie-F <princessmae.fleming@gmail.com>
2026-02-11 12:27:58 -0500

    part 3 - Rounding

--- Topic3VariablesAssignment/Program.cs
+++ Topic3VariablesAssignment/Program.cs
@@ -9,12 +9,16 @@
             double price = 24.95;
             string fullName = "Mercedes Fleming";
             int graduationYear = 2027;
+            int r = 3;
+            double area = (Math.PI * r * r);
             Console.WriteLine("Class is in room " + roomNumber + "!");
             Console.WriteLine("My favorite class is " + courseName + ".");
             Console.WriteLine("The price of my favorite book is " + price + ".");
             Console.WriteLine("    ");
             Console.WriteLine("My name is " + fullName + " and I will garduate in " + graduationYear + "!");
-            Console.WriteLine();
+            Console.WriteLine("    ");
+            Console.WriteLine(Math.PI * r * r); // Pi * R^2
+            Console.WriteLine("The area is " + area);
         }
     }
 }
