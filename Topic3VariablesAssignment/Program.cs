50cb1fe0e482d52a6cf05ae8619c5a1e372c9be6
362Sadie-F <princessmae.fleming@gmail.com>
2026-02-11 12:06:13 -0500

    Add project files.

---
+++ Topic3VariablesAssignment/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic3VariablesAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
